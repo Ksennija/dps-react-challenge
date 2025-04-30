11b37e71d34433a7c69070fdd2719552a60d4e15
Ksenia Burgucheva <kburgucheva@Ksenias-Air.fritz.box>
2025-04-30 09:17:07 +0200

    Display user data in a table

--- src/UsersTable/UsersTable.tsx
+++ src/UsersTable/UsersTable.tsx
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
-// import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-// import InvoiceStatus from '@/app/ui/invoices/status';
-// import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
+import { useEffect, useState } from 'react';
 import { fetchUsers } from '../api';
+import { User } from '../types';
+import styles from './UsersTable.module.css';
 
 const UsersTable: React.FC = () => {
 	//query: string | null
+	const [users, setUsers] = useState<User[]>([]);
+
 	useEffect(() => {
 		async function fetchAllUsers() {
-			const users = await fetchUsers();
+			const fetchedUsers = await fetchUsers();
+			setUsers(fetchedUsers);
 			console.log(users);
 		}
 
@@ -16,130 +18,30 @@ const UsersTable: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="mt-6 flow-root">
-			{/*
-			<div className="inline-block min-w-full align-middle">
-				<div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-					<div className="md:hidden">
-						{invoices?.map((invoice) => (
-							<div
-								key={invoice.id}
-								className="mb-2 w-full rounded-md bg-white p-4"
-							>
-								<div className="flex items-center justify-between border-b pb-4">
-									<div>
-										<div className="mb-2 flex items-center">
-											<p>{invoice.name}</p>
-										</div>
-										<p className="text-sm text-gray-500">
-											{invoice.email}
-										</p>
-									</div>
-									<InvoiceStatus status={invoice.status} />
-								</div>
-								<div className="flex w-full items-center justify-between pt-4">
-									<div>
-										<p className="text-xl font-medium">
-											{formatCurrency(invoice.amount)}
-										</p>
-										<p>{formatDateToLocal(invoice.date)}</p>
-									</div>
-									<div className="flex justify-end gap-2">
-										<UpdateInvoice id={invoice.id} />
-										<DeleteInvoice id={invoice.id} />
-									</div>
-								</div>
-							</div>
-						))}
+		<div className={styles.table}>
+			<div className={`${styles.row} ${styles.heading}`}>
+				<div className={styles.sell}>Name</div>
+				<div className={styles.sell}>City</div>
+				<div className={styles.sell}>Birthday</div>
+			</div>
+			{users?.map((user) => (
+				<div key={user.id} className={styles.row}>
+					<div className={styles.sell}>{user.name}</div>
+					<div className={styles.sell}>{user.city}</div>
+					<div className={styles.sell}>
+						{formatDate(user.birthday)}
 					</div>
-					<table className="hidden min-w-full text-gray-900 md:table">
-						<thead className="rounded-lg text-left text-sm font-normal">
-							<tr>
-								<th
-									scope="col"
-									className="px-4 py-5 font-medium sm:pl-6"
-								>
-									Customer
-								</th>
-								<th
-									scope="col"
-									className="px-3 py-5 font-medium"
-								>
-									Email
-								</th>
-								<th
-									scope="col"
-									className="px-3 py-5 font-medium"
-								>
-									Amount
-								</th>
-								<th
-									scope="col"
-									className="px-3 py-5 font-medium"
-								>
-									Date
-								</th>
-								<th
-									scope="col"
-									className="px-3 py-5 font-medium"
-								>
-									Status
-								</th>
-								<th
-									scope="col"
-									className="relative py-3 pl-6 pr-3"
-								>
-									<span className="sr-only">Edit</span>
-								</th>
-							</tr>
-						</thead>
-						<tbody className="bg-white">
-							{invoices?.map((invoice) => (
-								<tr
-									key={invoice.id}
-									className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
-								>
-									<td className="whitespace-nowrap py-3 pl-6 pr-3">
-										<div className="flex items-center gap-3">
-											<Image
-												src={invoice.image_url}
-												className="rounded-full"
-												width={28}
-												height={28}
-												alt={`${invoice.name}'s profile picture`}
-											/>
-											<p>{invoice.name}</p>
-										</div>
-									</td>
-									<td className="whitespace-nowrap px-3 py-3">
-										{invoice.email}
-									</td>
-									<td className="whitespace-nowrap px-3 py-3">
-										{formatCurrency(invoice.amount)}
-									</td>
-									<td className="whitespace-nowrap px-3 py-3">
-										{formatDateToLocal(invoice.date)}
-									</td>
-									<td className="whitespace-nowrap px-3 py-3">
-										<InvoiceStatus
-											status={invoice.status}
-										/>
-									</td>
-									<td className="whitespace-nowrap py-3 pl-6 pr-3">
-										<div className="flex justify-end gap-3">
-											<UpdateInvoice id={invoice.id} />
-											<DeleteInvoice id={invoice.id} />
-										</div>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
 				</div>
-			</div>{' '}
-			*/}
+			))}
 		</div>
 	);
 };
+
+function formatDate(date: string) {
+	const formattedDate = new Date(date);
+	return `${formattedDate.getDate()}.${
+		formattedDate.getMonth() + 1
+	}.${formattedDate.getFullYear()}`;
+}
 
 export default UsersTable;
